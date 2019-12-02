363adafeaddc6dddb10e16496cdb5bf3282ff308
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-02 09:00:44 -0500

    Add configuration settings for generators
      Disable generation of test files that we won't be using

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,15 @@ module Invoicer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    # disable some tests we don't need
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: true,
+      helper_specs: false,
+      routing_specs: false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
