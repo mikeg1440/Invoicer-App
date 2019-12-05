13a591d69d61aeacc55facbc30c1eea6c8e9d6b6
Michael Gaudreau <gaudreaum01@gmail.com>
2019-12-05 13:21:23 -0500

    Add a show partial for invoice and call it from the show view for invoices, also add a helper method in invoice model for calculating the totals of the invoice from the invoice_product lines

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,14 @@ class Invoice < ApplicationRecord
   belongs_to :account
   has_many :invoice_products
   has_many :products, through: :invoice_products
+
+  def sub_total
+    # require 'pry'; binding.pry
+    self.invoice_products.sum(&:total)
+  end
+
+  def grand_total
+    # if we add options for tax calculation this is were we would do it, for now its the same as sub total
+    self.invoice_products.sum(&:total)
+  end
 end
