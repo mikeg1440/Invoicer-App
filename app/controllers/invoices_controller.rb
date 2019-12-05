06b7d7aa0c09a4a91ff82d90b3d63b812f53370f
Michael Gaudreau <gaudreaum01@gmail.com>
2019-12-05 12:41:01 -0500

    Add code for the invoice index and show views along with the corresponding methods in the controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,12 +1,13 @@
 class InvoicesController < ApplicationController
   def index
-    @invoices = current_user.invoices 
+    @invoices = current_user.invoices
   end
 
   def new
   end
 
   def show
+    @invoice = current_user.invoices.find_by(id: params[:id])
   end
 
   def edit
