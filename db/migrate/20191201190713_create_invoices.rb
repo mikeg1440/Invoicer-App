d70c80d6c7a494c5132fe1103296a96128ba15ad
Michael Gaudreau <gaudreaum01@gmail.com>
2019-12-05 13:19:41 -0500

    Add a comment that might be used later for setting precision of amount_due in invoice table so we don't have to do it elsewhere later on

--- db/migrate/20191201190713_create_invoices.rb
+++ db/migrate/20191201190713_create_invoices.rb
@@ -1,10 +1,11 @@
 class CreateInvoices < ActiveRecord::Migration[6.0]
   def change
     create_table :invoices do |t|
-      t.integer :amount_due
+      t.integer :amount_due #, t.decimal :amount_due, precision: 2 
       t.datetime :due_time
       t.string :status
       t.integer :account_id
+
 
       t.timestamps
     end
