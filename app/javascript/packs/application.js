82da632b563e49c74443758b04b51f29e2b489c5
Michael Gaudreau <gaudreaum01@gmail.com>
2019-12-06 11:50:36 -0500

    Add a require line that we may not even need but it doesnt break anything so keep it until we merge back to master

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery_ujs
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
