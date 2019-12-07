f6c52e0bca14163cedc73635249c1da7a8d06092
Michael Gaudreau <gaudreaum01@gmail.com>
2019-12-07 12:27:30 -0500

    Add validations to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,12 @@ class Invoice < ApplicationRecord
   has_many :invoice_products
   has_many :products, through: :invoice_products
 
+  accepts_nested_attributes_for :invoice_products
+
+  validates :account_id, presence: :true
+  validates :due_time, presence: :true
+
+
   def sub_total
     # require 'pry'; binding.pry
     self.invoice_products.sum(&:total)
@@ -11,5 +17,12 @@ class Invoice < ApplicationRecord
   def grand_total
     # if we add options for tax calculation this is were we would do it, for now its the same as sub total
     self.invoice_products.sum(&:total)
+  end
+
+  def calculate_product_totals
+    binding.pry
+    self.invoice_products.each do |line|
+      line.total = line.product.price * line.quantity
+    end
   end
 end
