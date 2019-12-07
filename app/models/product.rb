ac2c2b8a12e38f71944afabc6641f8a19de37c60
Michael Gaudreau <gaudreaum01@gmail.com>
2019-12-07 12:33:41 -0500

    Add validations for product model attributes(name, description, price)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
 
   has_many :invoice_products
-  
+
+  validates :name, presence: :true, uniqueness: :true
+  validates :description, presence: :true
+  validates :price, presence: :true
+
 end
