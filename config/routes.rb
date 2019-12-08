9bdc705edca1197dd8e6ff696d9ae32a22cf7c7c
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-08 13:34:30 -0500

    Refactor signin and signout routes to be more DRY (/signin and /signout)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}
+  devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
