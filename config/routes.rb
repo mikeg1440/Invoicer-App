c91feb07f74b2107892612fd1b274bf37907c07f
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-09 14:26:41 -0500

    Refactor invoices routes to be nested within accounts

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,23 @@ Rails.application.routes.draw do
 
   root 'application#welcome'
   # resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  resources :accounts, only: [:show] do
+    resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
+
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   delete '/accounts/:id', to: 'accounts#destroy'
   resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   delete '/products/:id', to: 'products#destroy', as: "destroy_product"
-  resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+
 
   scope :users do
+    resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   end
 
-  # devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'} do
-  #   resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  # end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
