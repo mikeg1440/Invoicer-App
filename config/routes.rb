b1af9430360a934c4bc0bfadda58303a8595bdbd
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-08 19:15:20 -0500

    Refactor routes to allow invoices to be scoped inside or users

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  scope :users do
+    resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
