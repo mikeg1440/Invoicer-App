ce5f103a183ffdcec32f652f23044936c8a664e5
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-08 19:28:46 -0500

    Add routes for products using resources to clean up code

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
   root 'application#welcome'
-  resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  delete '/accounts/:id', to: 'accounts#destroy'
+  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
   scope :users do
     resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   end
+
+  # devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'} do
+  #   resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
