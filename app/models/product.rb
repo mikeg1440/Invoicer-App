f8c2db512e611a3085e1b4ff45b442aaddfe5dde
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-10 18:01:18 -0500

    Fix validation on product that was causing some seed operations to fail

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
   has_many :invoice_products
   belongs_to :user
 
-  validates :user_id, presence: true
   validates :name, presence: :true, uniqueness: :true
   validates :description, presence: :true
   validates :price, presence: :true
