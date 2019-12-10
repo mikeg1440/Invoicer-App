432dec9ea3478d514f9c65fcfa9e9697b16d9ab3
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-10 13:19:24 -0500

    Refactor create invoice migration to add precision to totals and a default value of 'unsent' to invoice status

--- db/migrate/20191201190713_create_invoices.rb
+++ db/migrate/20191201190713_create_invoices.rb
@@ -1,9 +1,9 @@
 class CreateInvoices < ActiveRecord::Migration[6.0]
   def change
     create_table :invoices do |t|
-      t.integer :amount_due #, t.decimal :amount_due, precision: 2 
+      t.integer :amount_due #, t.decimal :amount_due, precision: 2
       t.datetime :due_time
-      t.string :status
+      t.string :status, default: 'unsent'  # dont forget to add this
       t.integer :account_id
 
 
