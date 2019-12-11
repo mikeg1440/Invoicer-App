21edd3085f142ca0af8b6aadf6d8aa4d1ae52487
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-11 13:58:20 -0500

    Add new custom routes for sending, accepting and declining a invoice

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   get '/add_field', to: 'invoices#add_field', as: 'add_field'
   post '/add_field', to: 'invoices#add_field'
 
-  get '/send_invoice/:id', to: 'invoices#send_invoice', as: 'send_invoice'
+  get '/invoices/accept/:id', to: 'invoices#accept', as: 'accept_invoice'
+  get '/invoices/decline/:id', to: 'invoices#decline', as: 'decline_invoice'
+  get '/send_invoice/:id', to: 'invoice_mailer#send_invoice', as: 'send_invoice'
 
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
