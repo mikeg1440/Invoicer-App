ee991eddefaffdae304130a1cc44bbf7cf399850
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-13 15:21:48 -0500

    Refactor clients controller to be more DRY and work with nested attributes for account

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,6 @@
 class ClientsController < ApplicationController
   before_action :authenticate_user!
 
-
-
   def index
     @clients = Client.all
   end
@@ -13,22 +11,28 @@ class ClientsController < ApplicationController
 
   def new
     @client = Client.new
+    @client.accounts.build
   end
 
   def create
-    client = current_user.clients.find_or_build_by(client_params, current_user)
-    if client.valid?
-      client.save
-      flash[:notice] = "Client successfully created!"
-      redirect_to client_path(client)
-    else
+    @client = current_user.clients.find_or_build_by(client_params, current_user)
+    if params[:account]
+      @client.save
+      account = current_user.accounts.build(name: params[:account][:name], user: current_user, client: @client)
+      account.save
+    end
+
+    unless @client.valid?
       flash[:alert] = client.errors.full_messages
       render :new
+    @client.save
+    flash[:notice] = "Client successfully created!"
+    redirect_to client_path(@client)
     end
   end
 
   def edit
-    @client = current_user.clients.find_by(id: params[:id])
+    @client = current_user.clients.where(id: params[:id]).first
   end
 
   def update
@@ -43,12 +47,13 @@ class ClientsController < ApplicationController
   end
 
   def destroy
-    @client = Client.find_by(id: params[:id])
-    if @client.destroy
+    @client = current_user.clients.find_by(id: params[:id])
+    if @client && @client.destroy
       flash[:notice] = "Client deleted successfully!"
       redirect_to clients_path
     else
-      flash[:alert] = "Client could not be deleted!"
+      flash[:alert] = "You can only delete clients that YOU create!"
+      @client = Client.find_by(id: params[:id])
       render :show
     end
   end
@@ -56,7 +61,7 @@ class ClientsController < ApplicationController
   private
 
   def client_params
-    params.require(:client).permit(:name, :business_name, :email, :phone_number, :address)
+    params.require(:client).permit(:name, :business_name, :email, :phone_number, :address, account_attributes: [:name] )
   end
 
 end
