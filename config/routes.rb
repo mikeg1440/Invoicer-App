6217fcdc30f2697e1747faaac9b30ba836a431a5
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-17 14:45:35 -0500

    Refactor routes to work with the new `/invoices/unset` path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'application#welcome'
   # resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  get '/invoices/unsent', to: 'invoices#unsent', as: 'unsent_invoices'
 
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
@@ -15,13 +16,17 @@ Rails.application.routes.draw do
   get '/invoices/accept/:id', to: 'invoices#accept', as: 'accept_invoice'
   get '/invoices/decline/:id', to: 'invoices#decline', as: 'decline_invoice'
   get '/send_invoice/:id', to: 'invoice_mailer#send_invoice', as: 'send_invoice'
+  # resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  #delete '/products/:id', to: 'products#destroy', as: "destroy_product"
+
+
 
   scope :users do
     resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-    devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+    resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   end
+  devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path_names: { sign_in: 'login', sign_out: 'logout'}
 
 end
