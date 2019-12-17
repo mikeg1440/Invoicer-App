6118578af412670d01df41c7007d332b978c0aaa
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-17 10:53:51 -0500

    Add validation for invoice_product quantity must be >= 1

--- app/models/invoice_product.rb
+++ app/models/invoice_product.rb
@@ -5,6 +5,6 @@ class InvoiceProduct < ApplicationRecord
 
   validates :product_id, presence: :true
   validates :quantity, presence: :true
-
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 
 end
