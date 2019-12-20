c4b4c33e683b188ce8d371dde7ecdcdc1c29a05e
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-20 15:52:13 -0500

    Add a class level scope method to get and show the top 5 most used products

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,13 @@ class Product < ApplicationRecord
   validates :description, presence: :true
   validates :price, presence: :true
 
+
+  def self.top_five
+    products = {}
+    self.all.map do |product|
+      products[product] = InvoiceProduct.where(product_id: product.id).count
+    end
+    products.sort_by {|product, value| value}.last(5).reverse
+  end
+
 end
