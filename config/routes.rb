d78b4e9a58f62f70c9ecb47aaa0b1f7a28e2fdc4
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-20 15:31:52 -0500

    Refactor routes to DRY out code and clean up commented code

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,32 @@
 Rails.application.routes.draw do
 
   root 'application#welcome'
-  # resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   get '/invoices/unsent', to: 'invoices#unsent', as: 'unsent_invoices'
 
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :invoices, only: [:index, :new, :create, :show]
   end
 
-  resources :invoices, only: [:edit, :update, :destroy]
+  get '/products/top-five', to: 'products#top_five', as: 'top_five_products'
 
-  get '/test', to: 'clients#test'
-  post '/test', to: 'clients#test'
+  resources :invoices, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+
+  # routes for adding fields dynamically
   get '/add_field', to: 'invoices#add_field', as: 'add_field'
   post '/add_field', to: 'invoices#add_field'
 
+  # routes for accepting or rejecting a invoice
   get '/invoices/accept/:id', to: 'invoices#accept', as: 'accept_invoice'
   get '/invoices/decline/:id', to: 'invoices#decline', as: 'decline_invoice'
   get '/send_invoice/:id', to: 'invoice_mailer#send_invoice', as: 'send_invoice'
-  # resources :accounts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
-  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  #delete '/products/:id', to: 'products#destroy', as: "destroy_product"
-
-
 
   scope :users do
-    # resources :invoices, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-    get '/invoices', to: 'invoices#index', as: 'user_invoices'
-    get '/invoices/new', to: 'invoices#new', as: 'new_invoice'
-    # post '/invoices', to: 'invoices#create', as: 'invoices'
-
     resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    # next line will be for clients to view invoices
+    # get '/clients/invoices', to: 'clients#invoices', as: 'client_invoices'
   end
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}, path_names: { sign_in: 'login', sign_out: 'logout'}
 
