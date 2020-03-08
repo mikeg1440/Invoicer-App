e642047cfe38b4ce626a32b6a8c37819b976afb5
Mike Gaudreau <gaudreaum01@gmail.com>
2020-03-08 15:12:37 -0400

    Fix error in seed file by creating products with a user instad of trying to create products alone which voilates the db associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@
 Faker::Config.locale = 'en-US'
 
 # create default admin user
-User.create(username: 'admin', email: 'admin@mail.com', password: 'admin123', admin: true)
+admin = User.create(username: 'admin', email: 'admin@mail.com', password: 'admin123', admin: true)
 
 # create some products
-Product.create(name: 'Debugging', description: 'Identifying and removing errors from computer hardware or software', price: 20)
-Product.create(name: 'Incident Response', description: 'Organizing a approach to addressing and managing the aftermath of a security breach or cyberattack.', price: 150)
-Product.create(name: 'Code Review', description: 'Systematic examination of computer source code. It is intended to find and fix mistakes overlooked in the initial development phase, improving both the overall quality of software and the developers skills.', price: 35)
-Product.create(name: 'Web Accesiblity Management', description: 'Ensuring there are no barriers that prevent interaction with, or access to, websites on the World Wide Web by people with physical disabilities, situational disabilities, and socio-economic restrictions on bandwidth and speed.', price: 80)
-Product.create(name: 'Coding', description: 'The process of developing and implementing various sets of instructions to enable a computer to do a certain task.', price: 35)
-Product.create(name: 'Cyber Security Assesment', description: 'A risk-based approaches to examine and enhance defense mechanisms against cyber-attacks, helps identify threats that could affect the availability and reliability of a system.', price: 135)
-Product.create(name: 'Cyber Security Penetration Test', description: 'An authorized simulated cyberattack on a computer system, performed to evaluate the security of the system to expose vulnerabilities and holes in security.', price: 145)
+# admin.products.create(name: 'Debugging', description: 'Identifying and removing errors from computer hardware or software', price: 20)
+# admin.products.create(name: 'Incident Response', description: 'Organizing a approach to addressing and managing the aftermath of a security breach or cyberattack.', price: 150)
+# admin.products.create(name: 'Code Review', description: 'Systematic examination of computer source code. It is intended to find and fix mistakes overlooked in the initial development phase, improving both the overall quality of software and the developers skills.', price: 35)
+# admin.products.create(name: 'Web Accesiblity Management', description: 'Ensuring there are no barriers that prevent interaction with, or access to, websites on the World Wide Web by people with physical disabilities, situational disabilities, and socio-economic restrictions on bandwidth and speed.', price: 80)
+# admin.products.create(name: 'Coding', description: 'The process of developing and implementing various sets of instructions to enable a computer to do a certain task.', price: 35)
+# admin.products.create(name: 'Cyber Security Assesment', description: 'A risk-based approaches to examine and enhance defense mechanisms against cyber-attacks, helps identify threats that could affect the availability and reliability of a system.', price: 135)
+# admin.products.create(name: 'Cyber Security Penetration Test', description: 'An authorized simulated cyberattack on a computer system, performed to evaluate the security of the system to expose vulnerabilities and holes in security.', price: 145)
 
 
 
@@ -46,11 +46,10 @@ User.all.each do |user|
   account.save
 
   # create 3 products
-    # 3.times do
-    #   product = Product.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price, description: Faker::Lorem.sentence(word_count: 13))
-    #   product.user = user
-    #   product.save
-    # end
+    5.times do
+      product = user.products.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price, description: Faker::Lorem.sentence(word_count: 13))
+      product.save
+    end
 
 end
 
@@ -62,6 +61,7 @@ Invoice.all.each do |invoice|
     invoice_product = invoice.invoice_products.new
     invoice_product.product = Product.all.sample
     invoice_product.quantity = rand(10)
+    puts "Product Count: #{Product.all.count}"
     invoice_product.total = invoice_product.quantity * invoice_product.product.price
     invoice.save
   end
