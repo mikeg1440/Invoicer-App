b4f6ef7154ee1dca2e50db7031a21b280ce5c5f0
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-17 13:31:22 -0400

    Fix application config to only load Dotenv::Railtie if not in production evnironment

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
-
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -20,7 +21,7 @@ module Invoicer
       helper_specs: false,
       routing_specs: false
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
